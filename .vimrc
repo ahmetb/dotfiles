6b38c62e4a946e4cabbf07dbbb7d0751bf536010
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2017-07-08 21:50:38 -0700

    WIP: vim-go
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

--- .vimrc
+++ .vimrc
@@ -15,4 +15,11 @@ set ttymouse=xterm2
 set backspace=2 " make backspace work like most other apps
 set backspace=indent,eol,start
 
+" Specify a directory for plugins (for Neovim: ~/.local/share/nvim/plugged)
+call plug#begin('~/.vim/plugged')
+
+Plug 'fatih/vim-go'
+
+" Initialize plugin system
+call plug#end()
 
--- install_gotools.sh
+++ install_gotools.sh
@@ -8,7 +8,7 @@ GOPKGS=(
 	# vscode-go tools
 	github.com/nsf/gocode \
 	github.com/tpng/gopkgs \
-	github.com/lukehoban/go-outline \
+	github.com/ramya-rao-a/go-outline \
 	github.com/acroca/go-symbols \
 	golang.org/x/tools/cmd/guru \
 	golang.org/x/tools/cmd/gorename \
@@ -18,6 +18,18 @@ GOPKGS=(
 	sourcegraph.com/sqs/goreturns \
 	golang.org/x/tools/cmd/goimports \
 	github.com/fatih/gomodifytags \
+
+	# vim-go
+	github.com/jstemmer/gotags \
+	github.com/klauspost/asmfmt/cmd/asmfmt \
+	github.com/alecthomas/gometalinter \
+	golang.org/x/tools/cmd/goimports \
+	github.com/kisielk/errcheck \
+	github.com/fatih/motion \
+	github.com/fatih/gomodifytags \
+	github.com/zmb3/gogetdoc \
+	github.com/josharian/impl \
+	github.com/dominikh/go-tools/cmd/keyify \
 
 	# other go dev
 	github.com/kardianos/govendor \
