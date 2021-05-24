adf9364805c2443d206e58af752a05cc145d025c
Ahmet Alp Balkan <ahmetb@google.com>
2021-05-24 10:04:57 -0700

    updates
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- mkaudio.sh
+++ mkaudio.sh
@@ -2,10 +2,13 @@
 set -xeou pipefail
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
 rm -rf -- "${SCRIPT_DIR}/.audio"
 mkdir -p -- "${SCRIPT_DIR}/.audio"
 cd -- "${SCRIPT_DIR}/.audio"
+
+youtube-dl -q -f 251 'https://www.youtube.com/watch?v=7ivcvcWmOPI' -o- > sedat.in.opus
+ffmpeg -loglevel fatal -i sedat.in.opus -ss 0:00.20 -to 0:07.70 -f mp3 sedat.mp3
+rm sedat.in.opus
 
 youtube-dl -q -f 251 'https://www.youtube.com/watch?v=Up81uR5z88w' -o- > izmir.in.opus
 ffmpeg -loglevel fatal -i izmir.in.opus -ss 2:07 -to 2:22 -f mp3 izmir.mp3
@@ -18,10 +21,6 @@ rm plevne.opus
 youtube-dl -q -f 251 'https://www.youtube.com/watch?v=SZCCT2jauF8' -o- >  savas.in.webm
 ffmpeg -loglevel fatal -i savas.in.webm -ss 2:10 -to 2:28 -f mp3 savas.mp3
 rm savas.in.webm
-
-youtube-dl -q -f 251 'https://www.youtube.com/watch?v=NuT0Vd0d3qk' -o- > banzai.webm
-ffmpeg -loglevel fatal -i banzai.webm -ss 01:03 -to 01:13 -f mp3 banzai.mp3
-rm banzai.webm
 
 youtube-dl -q -f 251 https://www.youtube.com/watch\?v\=j6jL95BaSeM -o- > recep.m4a
 ffmpeg -loglevel fatal -i recep.m4a -ss 1:19 -to 1:31.50 -f mp3 recep.mp3
