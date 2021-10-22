09f63dfc65f2c5770158b3208a81021320430d62
Ahmet Alp Balkan <ahmetb@google.com>
2021-10-22 14:06:24 -0700

    updates
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- bitbar-plugins/yahoo_stock_ticker.1m.py
+++ bitbar-plugins/yahoo_stock_ticker.1m.py
@@ -20,7 +20,7 @@ import subprocess
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Enter your stock symbols here in the format: ["symbol1", "symbol2", ...]
-symbols = ["GOOG", "AMZN", "MSFT", "FB", "NVDA", "TSLA", "QQQ","SPY", "GLD", "TAN", "DIA", "NFLX", "AAPL", "ICLN", "ARKK", "GME","TRY=X","ABNB","SQ", "BTC-USD","ETH-USD", "COIN", "PTON", "UBER"]
+symbols = ["GOOG", "AMZN", "MSFT", "FB", "NVDA", "TSLA", "QQQ","SPY", "GLD", "TAN", "DIA", "NFLX", "AAPL", "ICLN", "ARKK", "GME","TRY=X","ABNB","SQ", "BTC-USD","ETH-USD", "COIN", "PTON", "UBER", "CRM", "HOOD", "NET", "TWTR"]
 
 # Enter the order how you want to sort the stock list:
 # 'name'                     : Sort alphabetically by name from A to Z
